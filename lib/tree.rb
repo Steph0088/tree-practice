698274103ee2e306573a1959e3e289df33c0e436
Stephanie Garcia <stephanie.garcia400@myci.csuci.edu>
2020-02-22 11:51:15 -0800

    add method

--- lib/tree.rb
+++ lib/tree.rb
@@ -18,20 +18,85 @@ class Tree
 
   # Time Complexity: 
   # Space Complexity: 
-  def add(key, value)
-    raise NotImplementedError
+  # def add(key, value)
+  #   if @root.nil?
+  #     @root = TreeNode.new(key, value)
+  #   else
+  #     current = @root
+  #     while true
+  #       if key < current.key
+  #         if !current.left.nil?
+  #         current = current.left
+  #         else 
+  #           current.left = TreeNode.new(key, value)
+  #           return
+  #         end
+  #       else
+  #         if !current.right.nil?
+  #           current = current.right
+  #         else
+  #           current.right = TreeNode.new(key,value)
+  #           return
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
+
+  def add_helper(current_node, key, value)
+    return TreeNode.new(key, value) if current_node.nil?
+
+    if key < current_node.key
+      current_node.left = add_helper(current_node.left, key, value)
+    else
+      current_node.right = add_helper(current_node.right, key, value)
+    end
+    return current_node
   end
+    
+  def add(key, value)
+    @root = add_helper(@root, key, value)
+  end
+
+
+
+
 
   # Time Complexity: 
   # Space Complexity: 
   def find(key)
-    raise NotImplementedError
+    if current.node.nil?
+     return TreeNode.new(key,value)
+    elsif current.node == key
+      return true
+    end
+  
+
+  
+  
+
   end
+ 
+      
+   
+  
+
+
+    
+
 
   # Time Complexity: 
   # Space Complexity: 
   def inorder
-    raise NotImplementedError
+    return list if current_node.nil?
+  end
+    
+  def inorder_helper(current_node, list)
+
+    inorder_helper(current_node.left,list)
+    list << { key: current_node.key, value: current_node.value }
+    inorder_helper(current_node.right, list)
+    return list
   end
 
   # Time Complexity: 
